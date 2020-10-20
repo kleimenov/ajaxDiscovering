f5d1acfc221364fd13b4077d1a059442f56dcca3
Иван <kleimenov416@gmail.com>
2020-10-20 11:33:10 -0400

    add testButton function, function retern plain text if button is clicked

--- ajax.js
+++ ajax.js
@@ -1,2 +1,18 @@
+//here I will get element by id
 const doc = document.getElementById('button');
-console.log(doc)
+
+
+
+//lets test our code, let build test function, this function will return 
+// 'Everything doing well!' if all right
+const testFunction = () => {
+    console.log('Everything doing well!');
+}
+
+
+//lets check our event listener
+document.getElementById('testButton').addEventListener('click', testFunction)
+
+
+//create event listener (click will be event)
+document.getElementById('button').addEventListener('click');
